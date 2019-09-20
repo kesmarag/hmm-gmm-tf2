cacf14b353669a53024eee20c4b741f6dedebeb4
Costas Smaragdakis <kesmarag@iacm.forth.gr>
2019-09-20 14:49:15 +0300

    update

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-from .hmm import HiddenMarkovModel
-from .dataset import DataSet, toy_example, new_left_to_right_hmm
+from .hmm import HiddenMarkovModel, toy_example, new_left_to_right_hmm
+from .dataset import DataSet
